40a54e60cb51be0651566eab5ffe708e8ee89199
hirotatsu <hirotatsuuu@gmail.com>
2019-02-16 19:08:39 +1300

    update photo size

--- docs/images/resize.py
+++ docs/images/resize.py
@@ -8,9 +8,14 @@ for f in Path(images_path).rglob('*.jpg'):
     print(f.name)
 
     img = cv2.imread(f.name)
-    print('original size:', img.shape, img.shape[0])  # original size: (400, 400, 3)
+    print('original size:', img.shape, img.shape[0])
+    size = os.path.getsize(f.name)
 
-    scaled = cv2.resize(img, dsize=(int(img.shape[1] / 4), int(img.shape[0] / 4)))
-    print('{} -> {}'.format(img.shape, scaled.shape))
-
-    cv2.imwrite(f.stem + '-thumb.jpg', scaled)
+    if (size > 100000):
+        scaled = cv2.resize(img, dsize=(int(img.shape[1] / 8), int(img.shape[0] / 8)))
+        print('{} -> {}'.format(img.shape, scaled.shape))
+        cv2.imwrite(f.stem + '-thumb.jpg', scaled)
+    else:
+        scaled = cv2.resize(img, dsize=(int(img.shape[1] / 4), int(img.shape[0] / 4)))
+        print('{} -> {}'.format(img.shape, scaled.shape))
+        cv2.imwrite(f.stem + '-thumb.jpg', scaled)
